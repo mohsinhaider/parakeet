2bcf20eb5b5e5fa447dc4455f1e658ac1746ff30
mohsinhaider <mohsinshaider@gmail.com>
2018-02-24 06:47:47 -0600

    Creating server and doing directory setup

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const MongoClient = require('mongodb').MongoClient;
+
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = 8000;
+
+app.listen(port, function () {
+    console.log("Server accepting requests on port 8000....");
+});
