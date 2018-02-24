492ba3201511f08684ed59166732d1d4da1d05bc
mohsinhaider <mohsinshaider@gmail.com>
2018-02-24 07:43:15 -0600

    Completed /video POST route to store video URLs

--- app/routes/video_routes.js
+++ app/routes/video_routes.js
@@ -1,5 +1,15 @@
 module.exports = function(app, db) {
-    app.get('/video', (req, res) => {
-        res.send('Success');
-    })
+    app.post('/video', (req, res) => {
+        const details = {
+            "videoURL": req.body.videoURL
+        };
+        db.collection('videos').insertOne(details, (err, result) => {
+            if (err) {
+                res.send({ 'error': 'An error has occured.' });
+            }
+            else {
+                res.send(result.ops[0]);
+            }
+        });
+    });
 };
