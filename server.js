51284d85e1cfe7d210f075185ccee9b849eff720
mohsinhaider <mohsinshaider@gmail.com>
2018-02-24 07:40:55 -0600

    Moved port listen logic into MongoClient connect callback

--- server.js
+++ server.js
@@ -1,13 +1,22 @@
 const express = require('express');
 const MongoClient = require('mongodb').MongoClient;
 
+const db = require('./config/db');
 const bodyParser = require('body-parser');
 
 const app = express();
 const port = 8000;
 
-require('./app/routes')(app, {});
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.listen(port, function () {
-    console.log("Server accepting requests on port 8000....");
+MongoClient.connect(db.uri, (err, client) => {
+    if (err) {
+        return console.log(err);
+    }
+    else {
+        require('./app/routes')(app, client.db('parakeet-db'));
+        app.listen(port, function () {
+            console.log("Server accepting requests on port 8000....");
+        });
+    }
 });
