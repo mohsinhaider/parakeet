8a4bc177ee70b80c6cf0df9efdaa0de427e96396
mohsinhaider <mohsinshaider@gmail.com>
2018-02-25 04:06:48 -0600

    Statically replacing input text after file selection

--- public/js/upload.js
+++ public/js/upload.js
@@ -1,9 +1,6 @@
 function readURL(input) {
     if (input.files && input.files[0]) {
         var textField = document.getElementById("upload-text");
-        textField.innerText = input.files[0].name;
+        textField.innerText = "Uploading... " + input.files[0].name;
     }
-
-    // POST to /video/transcribe
-    
 }
