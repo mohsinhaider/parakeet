c5912b620974bc6c5bfc17af85d1f86b0a4a2073
mohsinhaider <mohsinshaider@gmail.com>
2018-03-16 11:33:47 -0500

    Changes for ES6

--- public/js/watch.js
+++ public/js/watch.js
@@ -1,112 +1,123 @@
-var xmlHttp = new XMLHttpRequest();
+let searchButton = document.getElementById('search-button');
+let searchBar = document.getElementById('search-bar');
+let resultTable = document.getElementById('result-table');
+let videoPlayer = document.getElementById('video-player');
 
-var searchButton = document.getElementById('search-button');
-var searchBar = document.getElementById('search-bar');
-var resultTable = document.getElementById('result-table');
-var videoPlayer = document.getElementById('video-player');
+let currentTranscription = {};
 
-var currentTranscription = {};
+let xmlHttp = new XMLHttpRequest();
 
-xmlHttp.onreadystatechange = function() { 
-    if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
-        var videosResponseObject = JSON.parse(xmlHttp.response);
-        var resultTable = document.getElementById('result-table');
-        var recordingsTable = document.getElementById('recording-table');
+function loadLectures() {
+    xmlHttp.onreadystatechange = () => { 
+        if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
+            let videosResponseObject = JSON.parse(xmlHttp.response);
 
-        var dayOfWeekCodes = { 0: "Sunday", 1: "Monday", 2: "Tuesday", 3: "Wednesday", 4: "Thursday", 5: "Friday", 6: "Saturday" };
-        
-        function bindRecordingRowClick(row, index) {
-            row.onclick = function() {
-                clearSearchResultTable();
-                unhighlightCurrentRecordingRow();
+            let resultTable = document.getElementById('result-table');
+            let recordingsTable = document.getElementById('recording-table');
 
-                row.classList.toggle('active');
+            let dayOfWeekCodes = { 
+                0: "Sunday", 
+                1: "Monday", 
+                2: "Tuesday", 
+                3: "Wednesday", 
+                4: "Thursday", 
+                5: "Friday", 
+                6: "Saturday" 
+            };
+            
+            function bindRecordingRowClick(row, index) {
+                row.onclick = function() {
+                    clearSearchResultTable();
+                    unhighlightCurrentRecordingRow();
 
-                var videoSource = document.createElement('source');
-                videoSource.setAttribute('src', '../lectures/' + videosResponseObject[index].src);
-                currentTranscription = videosResponseObject[index].transcription;
+                    row.classList.toggle('active');
 
-                if (videoPlayer.childElementCount >= 1) {
-                    videoPlayer.removeChild(videoPlayer.firstChild);
-                } 
+                    let videoSource = document.createElement('source');
+                    videoSource.setAttribute('src', '../lectures/' + videosResponseObject[index].src);
+                    currentTranscription = videosResponseObject[index].transcription;
 
-                videoPlayer.appendChild(videoSource);
-                videoPlayer.load();
+                    if (videoPlayer.childElementCount >= 1) {
+                        videoPlayer.removeChild(videoPlayer.firstChild);
+                    } 
+
+                    videoPlayer.appendChild(videoSource);
+                    videoPlayer.load();
+                }
             }
-        }
 
-        for (var i = 0; i < videosResponseObject.length; i++) {
-            var currentIndex = i;
-            var recordingRow = recordingsTable.insertRow();
-            var recordingCell = recordingRow.insertCell();
-            recordingRow.setAttribute('class', 'recording-row')
+            for (let i = 0; i < videosResponseObject.length; i++) {
+                let recordingRow = recordingsTable.insertRow();
+                let recordingCell = recordingRow.insertCell();
+                recordingRow.setAttribute('class', 'recording-row')
 
-            var currentVideoDayUploaded = dayOfWeekCodes[new Date(videosResponseObject[i].date).getDay()];
-            var currentVideoDateArray = (videosResponseObject[i].date).split("-");
-            var currentVideoProperDate = currentVideoDateArray[1] + "/" + currentVideoDateArray[2] + "/" + currentVideoDateArray[0].slice(2);
-            
-            var currentVideoDayAndDateDiv = document.createElement('div');
-            currentVideoDayAndDateDiv.setAttribute('class', 'recording-upload-date');
-            currentVideoDayAndDateDiv.innerHTML = currentVideoDayUploaded + ", " + currentVideoProperDate;
+                let currentVideoDayUploaded = dayOfWeekCodes[new Date(videosResponseObject[i].date).getDay()];
+                let currentVideoDateArray = (videosResponseObject[i].date).split("-");
+                let currentVideoProperDate = currentVideoDateArray[1] + "/" + currentVideoDateArray[2] + "/" + currentVideoDateArray[0].slice(2);
+                
+                let currentVideoDayAndDateDiv = document.createElement('div');
+                currentVideoDayAndDateDiv.setAttribute('class', 'recording-upload-date');
+                currentVideoDayAndDateDiv.innerHTML = currentVideoDayUploaded + ", " + currentVideoProperDate;
 
-            var currentVideoTitleDiv = document.createElement('div');
-            currentVideoTitleDiv.setAttribute('class', 'recording-title');
-            currentVideoTitleDiv.innerHTML = videosResponseObject[i].title;
+                let currentVideoTitleDiv = document.createElement('div');
+                currentVideoTitleDiv.setAttribute('class', 'recording-title');
+                currentVideoTitleDiv.innerHTML = videosResponseObject[i].title;
 
-            recordingCell.appendChild(currentVideoDayAndDateDiv);
-            recordingCell.appendChild(currentVideoTitleDiv);
-            
-            bindRecordingRowClick(recordingRow, i);
+                recordingCell.appendChild(currentVideoDayAndDateDiv);
+                recordingCell.appendChild(currentVideoTitleDiv);
+                
+                bindRecordingRowClick(recordingRow, i);
+            }
         }
     }
+    xmlHttp.open("GET", "/video", true);
+    xmlHttp.send(null);
 }
 
-xmlHttp.open("GET", "/video", true);
-xmlHttp.send(null);
+function processSearchQuery() {
+    searchButton.onclick = function() {
+        clearSearchResultTable();
+        let searchQuery = searchBar.value;
 
-searchButton.onclick = function() {
-    clearSearchResultTable();
-    var searchQuery = searchBar.value;
+        if (searchQuery != "") {
+            searchQuery = searchQuery.toLowerCase();
+            let searchQueryArray = searchQuery.split(" ");
+            let currentTranscriptionResults = currentTranscription.results;
 
-    if (searchQuery != "") {
-        searchQuery = searchQuery.toLowerCase();
-        var searchQueryArray = searchQuery.split(" ");
-        var currentTranscriptionResults = currentTranscription.results;
+            for (let key in currentTranscriptionResults) {
+                let currentTimestamp = currentTranscriptionResults[key].timestamp;
+                let currentResult = currentTranscriptionResults[key].result;
 
-        for (var key in currentTranscriptionResults) {
-            var currentTimestamp = currentTranscriptionResults[key].timestamp;
-            var currentResult = currentTranscriptionResults[key].result;
-
-            if (searchQueryArray.length > 1) {
-                if (currentResult.includes(searchQuery)) {
-                    loadSearchResults(currentTimestamp, currentResult);
-                }
-            }
-            else {
-                var currentResultArray = currentResult.split(' ');
-
-                for (var j = 0; j < currentResultArray.length; j++) {
-                    if (searchQuery === currentResultArray[j]) {
+                if (searchQueryArray.length > 1) {
+                    if (currentResult.includes(searchQuery)) {
                         loadSearchResults(currentTimestamp, currentResult);
-                        break;
                     }
                 }
-            }   
+                else {
+                    let currentResultArray = currentResult.split(' ');
+
+                    for (let j = 0; j < currentResultArray.length; j++) {
+                        if (searchQuery === currentResultArray[j]) {
+                            loadSearchResults(currentTimestamp, currentResult);
+                            break;
+                        }
+                    }
+                }   
+            }
         }
     }
 }
 
 function loadSearchResults(currentTimestamp, currentResult) {
-    var resultRow = resultTable.insertRow();
+    let resultRow = resultTable.insertRow();
 
-    var resultTimestampCell = resultRow.insertCell();
+    let resultTimestampCell = resultRow.insertCell();
     resultTimestampCell.setAttribute('valign', 'top');
 
-    var resultContentCell = resultRow.insertCell();
+    let resultContentCell = resultRow.insertCell();
 
-    var timestampTotalSeconds = Math.floor(currentTimestamp / 1000)
-    var finalTimestamp = formatTime(timestampTotalSeconds);
-    var finalTimestampLinkWrapped = document.createElement('a');
+    let timestampTotalSeconds = Math.floor(currentTimestamp / 1000)
+    let finalTimestamp = formatTime(timestampTotalSeconds);
+    let finalTimestampLinkWrapped = document.createElement('a');
     finalTimestampLinkWrapped.setAttribute('href', '#');
     finalTimestampLinkWrapped.innerHTML = finalTimestamp;
 
@@ -119,16 +130,12 @@ function loadSearchResults(currentTimestamp, currentResult) {
     resultContentCell.innerHTML = currentResult;
 }
 
-function bindTimestampRowClick() {
+let formatTime = (secondsTime) => {
+    let seconds = secondsTime % 60;
+    let minutes = Math.floor((secondsTime % 3600) / 60);
+    let hours = Math.floor(secondsTime / 3600);
 
-}
-
-function formatTime(secondsTime) {
-    var seconds = secondsTime % 60;
-    var minutes = Math.floor((secondsTime % 3600) / 60);
-    var hours = Math.floor(secondsTime / 3600);
-
-    var finalTimestamp = "";
+    let finalTimestamp = "";
 
     if (hours > 0) {
         finalTimestamp += "" + hours + ":" + (minutes < 10 ? "0" : "");
@@ -140,16 +147,19 @@ function formatTime(secondsTime) {
     return finalTimestamp;
 }
 
-function clearSearchResultTable() {
-    var currentResultTableBody = document.getElementById('result-table-body');
-    var clearedResultTableBody = document.createElement('tbody');
+let clearSearchResultTable = () => {
+    let currentResultTableBody = document.getElementById('result-table-body');
+    let clearedResultTableBody = document.createElement('tbody');
     clearedResultTableBody.setAttribute('id', 'result-table-body');
     currentResultTableBody.parentNode.replaceChild(clearedResultTableBody, currentResultTableBody);
 }
 
-function unhighlightCurrentRecordingRow() {
-    var recordingTableRows = document.getElementsByClassName('recording-row');
+let unhighlightCurrentRecordingRow = () => {
+    let recordingTableRows = document.getElementsByClassName('recording-row');
     for (let index = 0; index < recordingTableRows.length; index++) {
         recordingTableRows[index].classList.remove('active');
     }
 }
+
+loadLectures();
+processSearchQuery();
