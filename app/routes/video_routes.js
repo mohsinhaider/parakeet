43905e6aaf813422158b5296f2e2d5722db70784
mohsinhaider <mohsinshaider@gmail.com>
2018-04-19 22:46:05 -0500

    Adding bookmark routes and functionality

--- app/routes/video_routes.js
+++ app/routes/video_routes.js
@@ -22,9 +22,42 @@ module.exports = function(app, db) {
     });
 
     app.post('/video/store/search', (req, res) => {
-        console.log(req.body.query);
+        let videoFileName = req.body.video;
+        let searchQuery = req.body.query;
+        let searchArray;
+
+        db.collection('searches').findOne({ 'src': videoFileName }, (err, document) => {
+            searchArray = document.searches;
+            searchArray.push(searchQuery);
+            db.collection('searches').updateOne(
+                { src: videoFileName },
+                { $set: { searches: searchArray } }, (err, document) => {
+                    if (err) { res.send({ 'error': 'An error has occured updating a searches document for this video.' }); }
+                })
+        });
     });
 
+    app.get('/video/store/bookmark/:src', (req, res) => {
+        db.collection('bookmarks').find({ src: req.params.src }).toArray((err, items) => {
+            res.send(items);
+        });
+    });
+
+    app.post('/video/store/bookmark', (req, res) => {
+        let videoFileName = req.body.video;
+        let bookmarkedTime = req.body.time;
+        let bookmarkArray;
+
+        db.collection('bookmarks').findOne({ 'src': videoFileName }, (err, document) => {
+            bookmarkArray = document.bookmarks;
+            bookmarkArray.push(bookmarkedTime);
+            db.collection('bookmarks').updateOne(
+                { src: videoFileName },
+                { $set: { bookmarks: bookmarkArray } }, (err, document) => {
+                    if (err) { res.send({ 'error': 'An error has occured updating a searches document for this video.' }); }
+                })
+        });
+    });
 
     app.post('/video/transcribe', (req, res) => {
         if (req.files) {
@@ -35,6 +68,10 @@ module.exports = function(app, db) {
             var videoSubject = req.body.videosubject;
             var videoCRN = req.body.videocrn;
             var videoDate = req.body.videodate;
+
+            db.collection('searches').insertOne({ src: currentFileName, searches: [] }, (err, document) => {
+                if (err) { res.send({ 'error': 'An error has occured creating a searches document for this video.' }); }
+            });
 
             const details = {
                 'src': currentFileName,
